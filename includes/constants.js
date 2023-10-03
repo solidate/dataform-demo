0a3e8be0f2b992b8ff992e71c2b1cc19559528d2
Keshav Kumar <keshv@google.com>
2023-10-03 12:06:48 +0000

    Initial push

---
+++ includes/constants.js
@@ -0,0 +1,17 @@
+const base_table = "final";
+const train_table = "train";
+const test_table = "test";
+const dataset_name = "german_credit_dataset";
+const split_column = "splits";
+const target = "creditability";
+const model_name = "bqml_logistics_classification_model";
+
+module.exports = {
+  base_table,
+  train_table,
+  test_table,
+  dataset_name,
+  split_column,
+  target,
+  model_name
+};
